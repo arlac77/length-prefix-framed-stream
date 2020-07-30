32b1a7ae82ce7e9e45fff701938c7b90dd2c7b8b
Markus Felten <Markus.Felten@gmx.de>
2020-07-30 13:21:30 +0200

    test: enc-dec

--- tests/basics-test.mjs
+++ tests/basics-test.mjs
@@ -3,5 +3,21 @@ import test from "ava";
 import { Decode, Encode } from "length-prefix-framed-stream";
 
 
-test("x", async t => {
+test("encode-decode", async t => {
+  const encode = new Encode();
+  const decode = new Decode();
+  
+  encode.pipe(decode);
+  
+  encode.push("Hallo");
+  encode.push("1");
+  encode.push("");
+
+  const messages = [];
+  
+  for await (const message from decode) {
+    messages.push(message);   
+  }
+
+  t.deepEqual(messages,["Hallo","1",""]);
 }); 
