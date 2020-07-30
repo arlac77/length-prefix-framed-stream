775cdd05b8c1b1833518f794ed37f49dd3a0d38f
Markus Felten <Markus.Felten@db.com>
2020-07-30 12:42:18 +0200

    feat: Decode & Encode

--- src/framed-stream.mjs
+++ src/framed-stream.mjs
@@ -1,6 +1,30 @@
-
 import { Transform } from "stream";
 
 export class Decoder extends Transform {
+  _transform(chunk, enc, cont) {
+    while (chunk.length > 0) {
+      if (this.buffer) {
+        chunk = Buffer.concat([this.buffer, chunk]);
+      }
+      if(chunk.length >= 4) {
+        const length = chunk.readUInt32BE(0);
+        const nextFrame = 4 + length;
+        if(chunk.length >= nextFrame) {
+          this.push(chunk.splice(4, nextFrame));
+          chunk = chunk.splice(nextFrame);
+        }
+      }
+    }
+    this.buffer = chunk;
+  }
 }
 
+export class Encoder extends Transform {
+  _transform(message, enc, cont) {
+    const prefix = Buffer.alloc(4);
+    prefix.writeUInt32BE(message.length, 0);
+    this.push(prefix);
+    this.push(message);
+    cont();
+  }
+}
