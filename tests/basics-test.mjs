3596be4640046adc7923fc675aae187719e53c2f
Markus Felten <Markus.Felten@db.com>
2020-08-10 13:01:36 +0200

    test: multiple decode

--- tests/basics-test.mjs
+++ tests/basics-test.mjs
@@ -25,3 +25,34 @@ test("encode-decode", async t => {
 
   t.deepEqual(messages,["Hello","1","", "9999999999"]);
 }); 
+
+test("encode-decode multiple", async t => {
+  async function * seq() {
+    yield "Hello";
+    yield "1";
+    yield "";
+    yield "9999999999";
+  }
+
+  const readable = Readable.from(seq(), { objectMode: true });
+  const encode = new Encode();
+  
+  const p = readable.pipe(encode);
+
+  const messages = [];
+
+  for(let i = 0; i < 3; i++) { 
+  const decode = new Decode({ objectMode: true, encoding: 'utf8' });
+
+  p.pipe(decode);
+ 
+  const m = [];
+  messages.push(m);
+
+  for await (const message of decode) {
+    m.push(message);   
+  }
+   }
+
+  t.deepEqual(messages,[["Hello","1","", "9999999999"],[],[]]);
+}); 
