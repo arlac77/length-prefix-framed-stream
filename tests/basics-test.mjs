a55bd4bd588c8c73f9dbea64c0e908be4c48bfcf
Markus Felten <Markus.Felten@gmx.de>
2020-08-18 17:50:56 +0200

    test: cleanup

--- tests/basics-test.mjs
+++ tests/basics-test.mjs
@@ -1,58 +1,51 @@
-
 import test from "ava";
 import { Readable } from "stream";
 import { Decode, Encode } from "length-prefix-framed-stream";
 
-test("encode-decode", async t => {
-  async function * seq() {
-    yield "Hello";
-    yield "1";
-    yield "";
-    yield "9999999999";
-  }
+const fragmentFixture = ["Hello", "1", "", "9999999999"];
 
-  const readable = Readable.from(seq(), { objectMode: true });
+async function* sequence(fragments) {
+  for(const f of fragments) {
+    yield f;
+  }
+}
+
+test("encode-decode", async t => {
+  const readable = Readable.from(sequence(fragmentFixture), { objectMode: true });
   const encode = new Encode();
-  const decode = new Decode({ objectMode: true, encoding: 'utf8' });
+  const decode = new Decode({ objectMode: true, encoding: "utf8" });
 
   readable.pipe(encode).pipe(decode);
- 
+
   const messages = [];
 
   for await (const message of decode) {
-    messages.push(message);   
+    messages.push(message);
   }
 
-  t.deepEqual(messages,["Hello","1","", "9999999999"]);
-}); 
+  t.deepEqual(messages, fragmentFixture);
+});
 
 test("encode-decode multiple", async t => {
-  async function * seq() {
-    yield "Hello";
-    yield "1";
-    yield "";
-    yield "9999999999";
-  }
-
-  const readable = Readable.from(seq(), { objectMode: true });
+  const readable = Readable.from(sequence(fragmentFixture), { objectMode: true });
   const encode = new Encode();
-  
+
   const p = readable.pipe(encode);
 
   const messages = [];
 
-  for(let i = 0; i < 3; i++) { 
-  const decode = new Decode({ objectMode: true, encoding: 'utf8' });
+  for (let i = 0; i < 3; i++) {
+    const decode = new Decode({ objectMode: true, encoding: "utf8" });
 
-  p.pipe(decode);
- 
-  const m = [];
-  messages.push(m);
+    p.pipe(decode);
 
-  for await (const message of decode) {
-    m.push(message);   
+    const m = [];
+    messages.push(m);
+
+    for await (const message of decode) {
+      m.push(message);
+    }
   }
-   }
 
-  t.deepEqual(messages,[["Hello","1","", "9999999999"],[],[]]);
-}); 
+  t.deepEqual(messages, [fragmentFixture, [], []]);
+});
